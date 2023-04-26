7b9effdcb868874221ab1ad49e394980e3488647
Rafael Schouten <rafaelschouten@gmail.com>
2023-04-26 16:26:08 +0200

    no PythonCall

--- install-python-deps.jl
+++ install-python-deps.jl
@@ -11,7 +11,6 @@ for p in ["pytorch", "torchvision"]
 end
 CondaPkg.add("opencv")
 CondaPkg.add_pip("segment-anything", version="@git+https://github.com/facebookresearch/segment-anything.git")
-using PythonCall
 using Downloads
 # Download model
 Downloads.download("https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth", joinpath(@__DIR__, "sam_vit_h_4b8939.pth"))
